e6bc4cf0226ede55828418d25ef6e3c400e8fa57
up201806461 <up201806461@fe.up.pt>
2021-06-02 00:32:05 +0100

    Changes by Filipe Recharte

---
+++ src/tasks/chord/AlertSuccessorTask.java
@@ -0,0 +1,25 @@
+package tasks.chord;
+
+import messages.chord.AlertSuccessorMessage;
+import peer.Peer;
+import peer.storage.StorageFile;
+import tasks.Task;
+
+import javax.net.ssl.SSLSocket;
+
+public class AlertSuccessorTask extends Task {
+    public AlertSuccessorTask(AlertSuccessorMessage message, Peer peer, SSLSocket socket) {
+        super(message, peer, socket);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("ALERT SUCCESSOR TASK");
+        AlertSuccessorMessage alert = (AlertSuccessorMessage) message;
+
+        peer.setPredecessor(alert.getPredecessor());
+        for (StorageFile file : alert.getDelegatedFiles()) {
+            peer.getScheduler().submit(() -> peer.restoreDelegatedFile(file));
+        }
+    }
+}
